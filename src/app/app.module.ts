a002e328130725065ff4e06c753e1390ea8c2c32
DavidHolkup <davidholkup@gmail.com>
2023-11-28 18:09:27 +0100

    Define main layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RootComponent } from './root/root.component';
+import { RewardsComponent } from './root/rewards/rewards.component';
+import { TasksComponent } from './root/tasks/tasks.component';
+import { TaskComponent } from './root/tasks/task/task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RootComponent,
+    RewardsComponent,
+    TasksComponent,
+    TaskComponent
   ],
   imports: [
     BrowserModule,
