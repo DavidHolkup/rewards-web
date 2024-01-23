f5afda29e174b0f8509d7faf1dfb6321a21da91f
DavidHolkup <davidholkup@gmail.com>
2024-01-23 21:47:35 +0100

    Create deploy script

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+npm run deploy
+
+cd dist/
+
+git init
+git remote add origin git@github.com:DavidHolkup/rewards.git
+git add .
+git commit -m "Deploying to GitHub Pages"
+git branch -M main
+git push -u origin main --force
+
+cd ..
+
